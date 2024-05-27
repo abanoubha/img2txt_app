98d7bb336bbbdd186876cc348bced8da46295da5
Abanoub Hanna <abanoubhannaa73@gmail.com>
2024-05-28 00:12:16 +0300

    ui: + info in the info screen/activity, fix: insets n padding

--- app/src/main/java/com/softwarepharaoh/img2txt/InfoActivity.kt
+++ app/src/main/java/com/softwarepharaoh/img2txt/InfoActivity.kt
@@ -1,22 +1,11 @@
 package com.softwarepharaoh.img2txt
 
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 
 class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_info)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
